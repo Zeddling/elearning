4396fd7eaf58e0d496ba63c16f8338438338a6c2
zeddling <victorzeddys@gmail.com>
2021-10-15 14:19:55 +0300

    Added course, lesson and unit models with their respective views

--- elearning/courses/tests.py
+++ elearning/courses/tests.py
@@ -1,16 +1,217 @@
 from courses import models
 from django.test import TestCase
 from django.urls import reverse
+from django_webtest import WebTest
 
-# Create your tests here.
+# Test constants
 TITLE_COURSE = "Test Course"
+TITLE_COURSE_SLUG = "test-course"
+TITLE_LESSON = "Test Lesson"
+TITLE_LESSON_SLUG = "test-lesson"
 TITLE_UNIT = "Test Unit"
 DESCRIPTION_COURSE = "This is a test course"
+DESCRIPTION_LESSON = "This is a test lesson"
 DESCRIPTION_UNIT = "This is a test unit"
+
+
 class CourseModelTests(TestCase):
     def test_course_str(self):
         course = models.Course(title=TITLE_COURSE, description=DESCRIPTION_COURSE)
         self.assertEqual(str(course), TITLE_COURSE)
+    
+    def test_on_save_slug_set(self):
+        category = models.Course.objects.create(title=TITLE_COURSE, description=DESCRIPTION_COURSE)
+        self.assertEqual(category.slug, TITLE_COURSE_SLUG)
+
+
+class CourseFormTests(WebTest):
+    def test_course_form_vaid_on_submit_redirects_successfully(self):
+        form = self.app.get(reverse('add_course')).form
+        form['title'] = TITLE_COURSE
+        form['description'] = DESCRIPTION_COURSE
+        res = form.submit().follow()
+        self.assertEqual(res.status_code, 200)
+
+
+class CourseFormPageTests(TestCase):
+    def test_form_page_template_used(self):
+        res = self.client.get(reverse('add_course'))
+        self.assertTemplateUsed(res, "courses/add_course.html")
+
+    def test_form_rendered(self):
+        res = self.client.get(reverse('add_course'))
+        self.assertIn(b'label', res.content)
+
+
+class CoursePageTests(TestCase):
+    def setUp(self):
+        self.course = models.Course.objects.create(
+            title=TITLE_COURSE,
+            description=DESCRIPTION_COURSE,
+        )
+        self.unit = models.Unit.objects.create(
+            course = self.course,
+            title = TITLE_UNIT,
+            description = DESCRIPTION_UNIT
+        )
+    
+    def tearDown(self) -> None:
+        self.course.delete()
+        self.unit.delete()
+    
+    def test_course_template_used(self):
+        res = self.client.get(reverse("course", args=[TITLE_COURSE_SLUG]))
+        self.assertTemplateUsed(res, "courses/course.html")
+    
+    def test_units_listed(self):
+       res = self.client.get(reverse("course", args=[TITLE_COURSE_SLUG]))
+       self.assertIn(bytes(TITLE_UNIT, encoding='utf8'), res.content)
+
+
+class IndexPageTests(TestCase):
+    def setUp(self):
+        self.course = models.Course.objects.create(
+            title=TITLE_COURSE,
+            description=DESCRIPTION_COURSE,
+        )
+    
+    def tearDown(self) -> None:
+        self.course.delete()
+    
+    def test_template_index_used(self):
+        res = self.client.get(reverse("index"))
+        self.assertTemplateUsed(res, "courses/index.html")
+    
+    def test_course_rendered(self):
+        res = self.client.get(reverse("index"))
+        self.assertIn(bytes(TITLE_COURSE, encoding='utf8'), res.content)
+
+
+class LessonModelTests(TestCase):
+    def setUp(self):
+        self.course = models.Course.objects.create(
+            title=TITLE_COURSE,
+            description=DESCRIPTION_COURSE,
+        )
+        self.unit = models.Unit.objects.create(
+            course = self.course,
+            title = TITLE_UNIT,
+            description = DESCRIPTION_UNIT
+        )
+    
+    def tearDown(self) -> None:
+        self.course.delete()
+        self.unit.delete()
+    def test_lesson_str(self):
+        lesson = models.Lesson(title=TITLE_LESSON, description=DESCRIPTION_LESSON)
+        self.assertEqual(str(lesson), TITLE_LESSON)
+    
+    def test_slug_created_on_save(self):
+        lesson = models.Lesson.objects.create(
+            title=TITLE_LESSON, 
+            description=DESCRIPTION_LESSON,
+            unit = self.unit
+        )
+        self.assertEqual(lesson.slug, TITLE_LESSON_SLUG)
+
+
+class LessonFormTests(WebTest):
+    def setUp(self):
+        self.course = models.Course.objects.create(
+            title=TITLE_COURSE,
+            description=DESCRIPTION_COURSE,
+        )
+        self.unit = models.Unit.objects.create(
+            course = self.course,
+            title = TITLE_UNIT,
+            description = DESCRIPTION_UNIT
+        )
+    
+    def tearDown(self) -> None:
+        self.course.delete()
+        self.unit.delete()
+    
+    def test_form_valid_submit_status_200(self):
+        #   Test form submit valid
+        form = self.app.get(reverse('add_lesson', args=[self.unit.id])).form
+        form['title'] = TITLE_LESSON
+        form['description'] = DESCRIPTION_LESSON
+        res = form.submit().follow()
+        self.assertEqual(res.status_code, 200)
+    
+    def test_form_valid_submit_unit_and_course_properly_saved(self):
+        #   Save form
+        form = self.app.get(reverse('add_lesson', args=[self.unit.id])).form
+        form['title'] = TITLE_LESSON
+        form['description'] = DESCRIPTION_LESSON
+        form.submit().follow()
+
+        #   Test lesson exists
+        lesson = models.Lesson.objects.get(title=TITLE_LESSON)
+        self.assertIsNotNone(lesson)
+
+        #   Test unit set
+        self.assertEqual(TITLE_LESSON, str(lesson.title))
+
+
+class LessonFormPageTests(TestCase):
+    def setUp(self):
+        self.course = models.Course.objects.create(
+            title=TITLE_COURSE,
+            description=DESCRIPTION_COURSE,
+        )
+        self.unit = models.Unit.objects.create(
+            course = self.course,
+            title = TITLE_UNIT,
+            description = DESCRIPTION_UNIT
+        )
+        self.lesson = models.Lesson.objects.create(
+            title=TITLE_LESSON, 
+            description=DESCRIPTION_LESSON
+        )
+    
+    def tearDown(self) -> None:
+        self.course.delete()
+        self.unit.delete()
+        self.lesson.delete()
+    
+    def test_form_page_template_used(self):
+        res = self.client.get(reverse('add_lesson', args=[self.unit.id]))
+        self.assertTemplateUsed(res, "courses/add_lesson.html")
+
+    def test_form_rendered(self):
+        res = self.client.get(reverse('add_lesson', args=[self.unit.id]))
+        self.assertIn(b'label', res.content)
+
+
+class LessonPageTests(TestCase):
+    def setUp(self):
+        self.course = models.Course.objects.create(
+            title=TITLE_COURSE,
+            description=DESCRIPTION_COURSE,
+        )
+        self.unit = models.Unit.objects.create(
+            course = self.course,
+            title = TITLE_UNIT,
+            description = DESCRIPTION_UNIT
+        )
+        self.lesson = models.Lesson.objects.create(
+            title=TITLE_LESSON, 
+            description=DESCRIPTION_LESSON
+        )
+    
+    def tearDown(self) -> None:
+        self.course.delete()
+        self.unit.delete()
+        self.lesson.delete()
+    
+    def test_template_lesson_used(self):
+        res = self.client.get(reverse('lesson', args=[TITLE_LESSON_SLUG]))
+        self.assertTemplateUsed(res, "courses/lesson.html")
+    
+    def test_lesson_info_displayed(self):
+        res = self.client.get(reverse('lesson', args=[TITLE_LESSON_SLUG]))
+        self.assertIn(bytes(TITLE_LESSON, encoding="utf8"), res.content)
 
 
 class UnitModelTests(TestCase):
@@ -19,20 +220,80 @@ class UnitModelTests(TestCase):
         self.assertEqual(str(unit), TITLE_UNIT)
 
 
-class IndexPageTests(TestCase):
+class UnitFormTests(WebTest):
     def setUp(self):
-        self.category = models.Course.objects.create(
+        self.course = models.Course.objects.create(
             title=TITLE_COURSE,
             description=DESCRIPTION_COURSE,
         )
     
     def tearDown(self) -> None:
-        self.category.delete()
+        self.course.delete()
     
-    def test_template_index_used(self):
-        res = self.client.get(reverse("index"))
-        self.assertTemplateUsed(res, "courses/index.html")
+    def test_form_valid_submit_status_200(self):
+        #   Test form submit valid
+        form = self.app.get(reverse('add_unit', args=[TITLE_COURSE_SLUG])).form
+        form['title'] = TITLE_UNIT
+        form['description'] = DESCRIPTION_UNIT
+        res = form.submit().follow()
+        self.assertEqual(res.status_code, 200)
     
-    def test_category_rendered(self):
-        res = self.client.get(reverse("index"))
-        self.assertIn(bytes(TITLE_COURSE, encoding='utf8'), res.content)
+    def test_form_valid_submit_unit_and_course_properly_saved(self):
+        #   Save form
+        form = self.app.get(reverse('add_unit', args=[TITLE_COURSE_SLUG])).form
+        form['title'] = TITLE_UNIT
+        form['description'] = DESCRIPTION_UNIT
+        res = form.submit().follow()
+
+        #   Test unit exists
+        unit = models.Unit.objects.get(title=TITLE_UNIT)
+        self.assertIsNotNone(unit)
+
+        #   Test course set
+        self.assertEqual(TITLE_COURSE, str(unit.course))
+
+
+class UnitFormPageTests(TestCase):
+    def setUp(self):
+        self.course = models.Course.objects.create(
+            title=TITLE_COURSE,
+            description=DESCRIPTION_COURSE,
+        )
+    
+    def tearDown(self) -> None:
+        self.course.delete()
+    
+    def test_form_page_template_used(self):
+        res = self.client.get(reverse('add_unit', args=[TITLE_COURSE_SLUG]))
+        self.assertTemplateUsed(res, "courses/add_unit.html")
+
+    def test_form_rendered(self):
+        res = self.client.get(reverse('add_unit', args=[TITLE_COURSE_SLUG]))
+        self.assertIn(b'label', res.content)
+
+
+class UnitPageTests(TestCase):
+    def setUp(self):
+        self.course = models.Course.objects.create(
+            title=TITLE_COURSE,
+            description=DESCRIPTION_COURSE,
+        )
+        self.unit = models.Unit.objects.create(
+            course = self.course,
+            title = TITLE_UNIT,
+            description = DESCRIPTION_UNIT
+        )
+    
+    def tearDown(self) -> None:
+        self.course.delete()
+        self.unit.delete()
+    
+    def test_unit_template_used(self):
+        res = self.client.get(reverse('unit', args=[self.unit.id]))
+        self.assertTemplateUsed(res, 'courses/unit.html')
+    
+    def test_unit_info_rendered(self):
+        res = self.client.get(reverse('unit', args=[self.unit.id]))
+        self.assertIn(bytes(TITLE_UNIT, encoding='utf8'), res.content)
+        self.assertIn(bytes(DESCRIPTION_UNIT, encoding='utf8'), res.content)
+
--- elearning/courses/views.py
+++ elearning/courses/views.py
@@ -1,9 +1,106 @@
-from courses import models
+from django.urls.base import reverse
+from courses import models, forms
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 
 
 # Create your views here.
+class CourseFormView(FormView):
+    form_class = forms.CourseForm
+    template_name = 'courses/add_course.html'
+    course_title_slug = None
+
+    def form_valid(self, form):
+        #   Get form data
+        title = form.cleaned_data['title']
+        description = form.cleaned_data['description']
+        
+        #   Create course
+        course = models.Course.objects.create(
+            title = title,
+            description = description,
+        )
+
+        #   Get slug from course
+        self.course_title_slug = course.slug
+        
+        return super().form_valid(form)
+
+    def get_success_url(self) -> str:
+        return reverse('course', args=[self.course_title_slug])
+
+
+class LessonFormView(FormView):
+    form_class = forms.LessonForm
+    template_name = 'courses/add_lesson.html'
+    #   URL parameter for lesson
+    slug_lesson = None
+    
+    def form_valid(self, form):
+        #   Get form data
+        title = form.cleaned_data['title']
+        description = form.cleaned_data['description']
+        
+        #   Create lesson
+        lesson = models.Lesson.objects.create(
+            title = title,
+            description = description,
+            unit = models.Unit.objects.get(id=self.kwargs['id'])
+        )
+
+        #   Get slug from lesson
+        self.slug_lesson = lesson.slug
+        
+        return super().form_valid(form)
+    
+    def get_success_url(self) -> str:
+        return reverse('lesson', args=[self.slug_lesson])
+
+
+class UnitFormView(FormView):
+    form_class = forms.UnitForm
+    template_name = 'courses/add_unit.html'
+    unit_id = None
+
+    #   Add course field to unit on valid
+    def form_valid(self, form):
+        #   Get form data
+        title = form.cleaned_data['title']
+        description = form.cleaned_data['description']
+        unit = models.Unit.objects.create(
+            title = title,
+            description = description,
+            course = models.Course.objects.get(slug=self.kwargs['course_title_slug'])
+        )
+
+        self.unit_id = unit.id
+        return super().form_valid(form)
+    
+    def get_success_url(self) -> str:
+        return reverse('unit', args=[self.unit_id])
+        
+       
+def course_page(request, course_title_slug):
+    context_dict = {}
+    course = models.Course.objects.get(slug=course_title_slug)
+    context_dict['course'] = course
+    context_dict['units'] = models.Unit.objects.filter(course=course)
+    return render(request, "courses/course.html", context_dict)
+
 def index(request):
     context_dict = {}
     context_dict["courses"] = models.Course.objects.all()
     return render(request, "courses/index.html", context_dict)
+
+#   id refers to the unit id where the lesson belongs
+def lesson_page(request, lesson_title_slug):
+    context_dict = {}
+    context_dict['lesson'] = models.Lesson.objects.get(slug=lesson_title_slug)
+    return render(request, "courses/lesson.html", context_dict)
+
+def unit_page(request, id):
+    context_dict = {}
+    unit = models.Unit.objects.get(id=id)
+    context_dict['unit'] = unit
+    return render(request, "courses/unit.html", context_dict)
