e2eee3583110b40e8d2d19ea89c8368969f5e62e
zeddling <victorzeddys@gmail.com>
2021-10-18 12:27:35 +0300

    Initial commit with teacher app

--- elearning/courses/admin.py
+++ elearning/courses/admin.py
@@ -1,3 +1,7 @@
+from . import models
 from django.contrib import admin
 
 # Register your models here.
+admin.register(models.Course)
+admin.register(models.Lesson)
+admin.register(models.Unit)
--- elearning/elearning/urls.py
+++ elearning/elearning/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('course', include('courses.urls'))
+    path('course', include('courses.urls')),
+    path('teacher', include('teacher.urls')),
 ]
